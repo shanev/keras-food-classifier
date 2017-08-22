39cae1f9b579d203faed72310bfd1c2be63d1481
Shane Vitarana <shanev@gmail.com>
2017-08-22 00:12:27 -0400

    Exporting model

--- convert.py
+++ convert.py
@@ -1,9 +1,16 @@
+# coreml_model = coremltools.converters.keras.convert(model, input_names = 'image',
+# ... image_input_names = 'image', class_labels = ['cat', 'dog', 'rat'])
+
 import coremltools
-coreml_model = coremltools.converters.keras.convert('vege_model_1.h5')
+from keras.models import load_model
 
-# from keras.models import load_model
+model = load_model('vege_model_1.h5')
+model.summary()
 
-# model = load_model('first_try.h5')
+coreml_model = coremltools.converters.keras.convert(
+  model,
+  input_names = 'image',
+  image_input_names = 'image',
+  class_labels = ['carrots', 'kale'])
 
-# coreml_model = coremltools.converters.keras.convert(model)
 coreml_model.save("vege_model_1.mlmodel")
