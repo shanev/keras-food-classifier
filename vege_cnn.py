c460995597a1dec55909c087f1889be2da5c1d30
Shane Vitarana <shanev@gmail.com>
2017-08-22 19:41:33 -0400

    Smaller epochs

--- vege_cnn.py
+++ vege_cnn.py
@@ -48,9 +48,9 @@ img_width, img_height = 150, 150
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
-epochs = 50
+nb_train_samples = 1537
+nb_validation_samples = 181
+epochs = 25
 batch_size = 16
 
 if K.image_data_format() == 'channels_first':
