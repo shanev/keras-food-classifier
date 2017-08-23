ce48a821831c175c3c1386a336776a2a5cc6171f
Shane Vitarana <shanev@gmail.com>
2017-08-22 20:55:54 -0400

    Export somewhat working

--- convert.py
+++ convert.py
@@ -1,42 +1,50 @@
-import argparse
-import os
-import sys
+# import argparse
+# import os
+# import sys
 
 import coremltools
 from keras.models import load_model
 
-def testLoadModel(filename):
-    model = load_model(filename)
+# def testLoadModel(filename):
+#     model = load_model(filename)
 
-def convert(inFilename, outFileName, labels, author = '', license = '', short_description = ''):
-    coreml_model = coremltools.converters.keras.convert(inFilename,class_labels = labels, input_names = 'image', image_input_names = 'image')
-    coreml_model.author = author
-    coreml_model.license = license
-    coreml_model.short_description = short_description
-    coreml_model.save(outFilename)
+# def convert(inFilename, outFileName, labels, author = '', license = '', short_description = ''):
+#     coreml_model = coremltools.converters.keras.convert(inFilename,class_labels = labels, input_names = 'image', image_input_names = 'image')
+#     coreml_model.author = author
+#     coreml_model.license = license
+#     coreml_model.short_description = short_description
+#     coreml_model.save(outFilename)
 
-if __name__=="__main__":
-    a = argparse.ArgumentParser()
-    a.add_argument('--infile','-i', metavar="H5", default="model.h5")
-    a.add_argument('--outfile','-o', metavar="MLMODEL", default="model.mlmodel")
-    a.add_argument('--label','-l', metavar="LABEL", action='append')
-    a.add_argument('--labelfile','-f', metavar="FILENAME")
-    a.add_argument('--author','-a', metavar="NAME", default='')
-    a.add_argument('--license', metavar="LICENSE", default='Apache-2.0')
-    a.add_argument('--description','-d', metavar="SHORT DESCRIPTION", default='Model for image recognition')
+# if __name__=="__main__":
+#     a = argparse.ArgumentParser()
+#     a.add_argument('--infile','-i', metavar="H5", default="model.h5")
+#     a.add_argument('--outfile','-o', metavar="MLMODEL", default="model.mlmodel")
+#     a.add_argument('--label','-l', metavar="LABEL", action='append')
+#     a.add_argument('--labelfile','-f', metavar="FILENAME")
+#     a.add_argument('--author','-a', metavar="NAME", default='')
+#     a.add_argument('--license', metavar="LICENSE", default='Apache-2.0')
+#     a.add_argument('--description','-d', metavar="SHORT DESCRIPTION", default='Model for image recognition')
 
-    args = a.parse_args()
-    if args.infile is None or args.outfile is None:
-        a.print_help()
-        sys.exit(1)
-    elif (not os.path.exists(args.infile)):
-        print("Filename " + args.infile + " does not exist")
-        sys.exit(1)
-    elif args.label is None and args.labelfile is None:
-        print("You need either --label OR --labelfile with a list of files")
-        sys.exit(1)
+#     args = a.parse_args()
+#     if args.infile is None or args.outfile is None:
+#         a.print_help()
+#         sys.exit(1)
+#     elif (not os.path.exists(args.infile)):
+#         print("Filename " + args.infile + " does not exist")
+#         sys.exit(1)
+#     elif args.label is None and args.labelfile is None:
+#         print("You need either --label OR --labelfile with a list of files")
+#         sys.exit(1)
 
-    if (not args.label is None):
-        labels = args.label
+#     if (not args.label is None):
+#         labels = args.label
 
-    convert(args.infile, args.outfile, labels, args.author, args.license, args.description)
+#     convert(args.infile, args.outfile, labels, args.author, args.license, args.description)
+
+coreml_model = coremltools.converters.keras.convert(
+  'vege_model_1.h5',
+  input_names = 'image',
+  image_input_names = 'image',
+  class_labels = ['classLabel'])
+
+coreml_model.save('vege_model_1.mlmodel')
