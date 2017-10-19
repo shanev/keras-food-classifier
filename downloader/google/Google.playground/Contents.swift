eec320a315a982c5142bdcb3a989fa9d0cb4a500
Shane Vitarana <shanev@gmail.com>
2017-10-19 14:04:26 -0400

    cleaned up

--- downloader/google/Google.playground/Contents.swift
+++ downloader/google/Google.playground/Contents.swift
@@ -2,14 +2,9 @@
 
 import Foundation
 import PlaygroundSupport
-import UIKit
-
-var str = "Hello, playground"
 
 let googleUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCle3vkXuwf0ET-UgCqAWz8NlojY_WV-NE&cx=014498682126902798498:sqnzmufwthi&q=potato"
 let url = URL(string: googleUrl)!
-
-// let peopleNamedTom = arrayOfPeople.filter { $0["Name"] == "Tom" }
 
 URLSession.shared.dataTask(with: url) { (data, response, error) in
   if let data = data {
